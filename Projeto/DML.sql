38008b908cb0d68c82208992bf3bbc1b8912c3c0
MoreiraLopes <goncalorcml@ua.pt>
2023-05-20 03:00:17 +0100

    o

--- Projeto/DML.sql
+++ Projeto/DML.sql
@@ -1,22 +1,27 @@
 INSERT INTO Higiliquidos.Pessoa(NIF, nome, Data_de_Nascimento, email, morada, ContactoTelefonico) VALUES
-(294455765, "João Antonio", "05/10/1983", 'joaoant@gmail.com', "Rua Vou AliJaVenho", "931764532"),
+(294455765, "João Antonio", "05/10/1983", 'joaoant@gmail.com', "Rua Vou AliJaVenho", 931764532),
 (287649264, "Maria Silva", "12/03/1990", 'maria.silva@gmail.com', "Avenida Principal", "987654321"),
-(987654321, "Pedro Santos", "07/07/1985", 'pedro.santos@gmail.com', "Rua das Flores", "777888999"),
-(295748365, "Ana Sousa", "22/09/1995", 'ana.sousa@gmail.com', "Travessa das Oliveiras", "111222333"),
-(111111111, "Carlos Costa", "19/05/1978", 'carlos.costa@gmail.com', "Rua dos Amigos", "555444333"),
-(222222222, "Sofia Rodrigues", "14/08/1992", 'sofia.rodrigues@gmail.com', "Avenida Central", "666777888"),
-(999999999, "Rita Pereira", "28/11/1989", 'rita.pereira@gmail.com', "Praça da Liberdade", "222333444"),
-(888888888, "Luis Mendes", "02/02/1987", 'luis.mendes@gmail.com', "Rua Nova", "777111333"),
+(987654321, "Pedro Santos", "07/07/1985", 'pedro.santos@gmail.com', "Rua das Flores", "977888999"),
+(295748365, "Ana Sousa", "22/09/1995", 'ana.sousa@gmail.com', "Travessa das Oliveiras", "911022333"),
+(111111111, "Carlos Costa", "19/05/1978", 'carlos.costa@gmail.com', "Rua dos Amigos", "955444333"),
+(222222222, "Sofia Rodrigues", "14/08/1992", 'sofia.rodrigues@gmail.com', "Avenida Central", "966777888"),
+(999999999, "Rita Pereira", "28/11/1989", 'rita.pereira@gmail.com', "Praça da Liberdade", "222330444"),
+(888888888, "Luis Mendes", "02/02/1987", 'luis.mendes@gmail.com', "Rua Nova", "977111333"),
 (777777777, "Andre Almeida", "15/06/1998", 'andre.almeida@gmail.com', "Travessa dos Cravos", "999444555"),
-(666666666, "Marta Gomes", "10/09/1993", 'marta.gomes@gmail.com', "Avenida da Paz", "123456789"),
-(444444444, "Ricardo Ferreira", "25/12/1980", 'ricardo.ferreira@gmail.com', "Rua dos Sonhos", "444555666"),
-(333333333, "Inês Pereira", "18/07/1996", 'ines.pereira@gmail.com', "Praça dos Leões", "789123456"),
-(222222333, "Paulo Marques", "30/03/1984", 'paulo.marques@gmail.com', "Rua das Gaivotas", "987654321"),
-
-
-
-
-
+(666666666, "Marta Gomes", "10/09/1993", 'marta.gomes@gmail.com', "Avenida da Paz", "923456789"),
+(444444444, "Ricardo Ferreira", "25/12/1980", 'ricardo.ferreira@gmail.com', "Rua dos Sonhos", "944555666"),
+(333333333, "Inês Pereira", "18/07/1996", 'ines.pereira@gmail.com', "Praça dos Leões", "989123456"),
+(222222333, "Paulo Marques", "30/03/1984", 'paulo.marques@gmail.com', "Rua das Gaivotas", "987684321"),
+(123456789, "Patricia Santos", "08/06/1991", 'patricia.santos@gmail.com', "Avenida das Flores", "955666777"),
+(555666777, "Hugo Carvalho", "14/02/1988", 'hugo.carvalho@gmail.com', "Rua das Estrelas", "999888777"),
+(777888999, "Mariana Costa", "03/11/1994", 'mariana.costa@gmail.com', "Travessa dos Lírios", "911222333"),
+(999888777, "Rui Oliveira", "27/07/1986", 'rui.oliveira@gmail.com', "Avenida dos Pinheiros", "944505666"),
+(111222333, "Carolina Fernandes", "19/04/1997", 'carolina.fernandes@gmail.com', "Rua dos Girassóis", "977808999"),
+(444555666, "Diogo Pereira", "13/09/1992", 'diogo.pereira@gmail.com', "Travessa das Margaridas", "222333444"),
+(666555444, "Sara Rodrigues", "06/12/1984", 'sara.rodrigues@gmail.com', "Avenida das Águias", "933444555"),
+(333444555, "Gustavo Silva", "21/01/1990", 'gustavo.silva@gmail.com', "Rua das Árvores", "966877888"),
+(888777666, "Luana Mendonça", "17/08/1993", 'luana.mendonca@gmail.com', "Praça dos Girafas", "999000111"),
+(222330444, "Rodrigo Almeida", "29/05/1987", 'rodrigo.almeida@gmail.com', "Avenida dos Cravos", "944333222");
 
 
 
