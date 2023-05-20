e656495dbcdc04fb5e932802a5d8c3660afe7348
MoreiraLopes <goncalorcml@ua.pt>
2023-05-20 02:47:51 +0100

    o

--- Projeto/DDL.sql
+++ Projeto/DDL.sql
@@ -1,56 +1,223 @@
-Use p8g8
-GO
+use p8g8;
 
+
+DROP TABLE IF EXISTS Higiliquidos.Produto;
+DROP TABLE IF EXISTS Higiliquidos.Armazem;
 DROP TABLE IF EXISTS Higiliquidos.Carrinha;
+DROP TABLE IF EXISTS Higiliquidos.Pessoa;
+DROP TABLE IF EXISTS Higiliquidos.Empresa;
+DROP TABLE IF EXISTS Higiliquidos.Fornecedor;
+DROP TABLE IF EXISTS Higiliquidos.Encomenda;
+DROP TABLE IF EXISTS Higiliquidos.Encomenda_Fornecedor;
+DROP TABLE IF EXISTS Higiliquidos.Cliente;
+DROP TABLE IF EXISTS Higiliquidos.Encomenda_Cliente;  
+DROP TABLE IF EXISTS Higiliquidos.Funcionario;
+DROP TABLE IF EXISTS Higiliquidos.Vendedor;
+DROP TABLE IF EXISTS Higiliquidos.Distribuidor;
+DROP TABLE IF EXISTS Higiliquidos.Encomenda_Distribuidor;
+DROP TABLE IF EXISTS Higiliquidos.Carrinha_Distribuidor;
+DROP TABLE IF EXISTS Higiliquidos.Gerente;
+DROP TABLE IF EXISTS Higiliquidos.Encomenda_Produto;
+
+
+
+CREATE TABLE Higiliquidos.Produto (
+  ID INT NOT NULL,
+  nome VARCHAR(64) NOT NULL,
+  marca VARCHAR(64) NOT NULL,
+  Tipo_de_Produto VARCHAR(64),
+  IVA DECIMAL(5, 2) NOT NULL,
+  Preco DECIMAL(10, 2) NOT NULL,
+  quantidade INT NOT NULL,
+
+  PRIMARY KEY (ID)
+
+);
+
+
+CREATE TABLE Higiliquidos.Armazem (
+  ID INT NOT NULL,
+  tamanho INT NOT NULL,
+  endereco varchar(64) NOT NULL,
+  ID_Produto INT NOT NULL,
+  
+  PRIMARY KEY (ID),
+  FOREIGN KEY (ID_Produto) REFERENCES Higiliquidos.Produto(ID)
+  
+);
+
 
 CREATE TABLE Higiliquidos.Carrinha (
   matricula varchar(64) NOT NULL,
   ano INT NOT NULL,
   marca varchar(32) NOT NULL,
   combustivel varchar(32) NOT NULL,
-  peso INT,
+  peso INT NOT NULL,
 
-  PRIMARY KEY ('matricula')
-)
-GO
+  PRIMARY KEY (matricula)
+);
 
-DROP TABLE IF EXISTS Higiliquidos.Empresa;
+
+CREATE TABLE Higiliquidos.Pessoa (
+  nome varchar(64) NOT NULL,
+  email varchar(64) NOT NULL,
+  morada varchar(64) NOT NULL,
+  ContactoTelefonico INT NOT NULL,
+  NIF INT NOT NULL,
+  Data_de_Nascimento DATE NOT NULL,
+  
+  PRIMARY KEY (NIF)
+);  
+
 
 CREATE TABLE Higiliquidos.Empresa (
   nome varchar(64) NOT NULL,
   email varchar(64) NOT NULL,
   endereco varchar(64) NOT NULL,
   ContactoTelefonico INT NOT NULL,
+  ID_Armazem INT NOT NULL,
+  NIF_Pessoa INT NOT NULL,
+  matricula_Carrinha varchar(64) NOT NULL,
 
-  PRIMARY KEY ('nome')
-)
-GO
+  PRIMARY KEY (nome),
+  FOREIGN KEY (ID_Armazem) REFERENCES Higiliquidos.Armazem(ID),
+  FOREIGN KEY (NIF_Pessoa) REFERENCES Higiliquidos.Pessoa(NIF),
+  FOREIGN KEY (matricula_Carrinha) REFERENCES Higiliquidos.Carrinha(matricula)
+);
 
-DROP TABLE IF EXISTS Higiliquidos.Armazem;
 
-CREATE TABLE Higiliquidos.Armazem (
-  id INT NOT NULL,
-  tamanho INT,
-  endereco varchar(64) NOT NULL,
-  
-  PRIMARY KEY ('id')
-)
-GO
+CREATE TABLE Higiliquidos.Fornecedor (
+  NIF_Fornecedor INT NOT NULL,
 
-DROP TABLE IF EXISTS Higiliquidos.Pessoa;
+  PRIMARY KEY (NIF_Fornecedor),
+  FOREIGN KEY (NIF_Fornecedor) REFERENCES Higiliquidos.Pessoa(NIF)
 
-CREATE TABLE Higiliquidos.Pessoa (
-  id INT NOT NULL,
-  nome varchar(64) NOT NULL,
-  email varchar(64) NOT NULL,
-  morada varchar(64) NOT NULL,
-  ContactoTelefonico INT NOT NULL,
-  NIF INT,
-  Data_de_Nascimento DATE,
-  
-  PRIMARY KEY ('id')
-)
-GO
+);
+
+
+CREATE TABLE Higiliquidos.Encomenda (
+  ID INT PRIMARY KEY,
+  Data_Entrega VARCHAR(255),
+  ID_Produto INT NOT NULL,
+  Quantidade_Produto INT NOT NULL,
+  Quantidade_Total INT NOT NULL,
+
+  PRIMARY KEY (ID),
+  FOREIGN KEY (ID_Produto) REFERENCES Higiliquidos.Produto(ID),
+  FOREIGN KEY (Quantidade_Produto) REFERENCES Higiliquidos.Produto(quantidade)
+);
+
+
+CREATE TABLE Higiliquidos.Encomenda_Fornecedor (
+  NIF_Fornecedor INT NOT NULL,
+  ID_Encomenda INT NOT NULL,
+
+  PRIMARY KEY (ID_Encomenda),
+  FOREIGN KEY (NIF_Fornecedor) REFERENCES Higiliquidos.Fornecedor(NIF_Fornecedor),
+  FOREIGN KEY (ID_Encomenda) REFERENCES Higiliquidos.Encomenda(ID)
+
+);
+
+
+CREATE TABLE Higiliquidos.Cliente (
+  NIF_Cliente INT NOT NULL,
+
+  PRIMARY KEY (NIF_Cliente),
+  FOREIGN KEY (NIF_Cliente) REFERENCES Higiliquidos.Pessoa(NIF)
+);
+
+
+CREATE TABLE Higiliquidos.Encomenda_Cliente (
+  NIF_Cliente INT NOT NULL,
+  ID_Encomenda INT NOT NULL,
+
+  PRIMARY KEY (ID_Encomenda),
+  FOREIGN KEY (NIF_Cliente) REFERENCES Higiliquidos.Cliente(NIF_Cliente),
+  FOREIGN KEY (ID_Encomenda) REFERENCES Higiliquidos.Encomenda(ID)
+
+);
+
+
+CREATE TABLE Higiliquidos.Funcionario (
+  IBAN INT NOT NULL,
+  Numero_SS INT NOT NULL,
+  Data_Inicio_Atividade DATE NOT NULL,
+  NIF_Funcionario INT NOT NULL,
+  FOREIGN KEY (NIF_Funcionario) REFERENCES Higiliquidos.Pessoa(NIF),
+  PRIMARY KEY (NIF_Funcionario)
+);
+
+
+CREATE TABLE Higiliquidos.Vendedor (
+  Permissao_Venda BOOLEAN NOT NULL,
+  NIF_Vendedor INT NOT NULL,
+
+  FOREIGN KEY (NIF_Vendedor) REFERENCES Higiliquidos.Funcionario(NIF_Funcionario),
+  PRIMARY KEY (NIF_Vendedor)
+);
+
+
+CREATE TABLE Higiliquidos.Distribuidor (
+  Permissao_Distribuir BOOLEAN NOT NULL,
+  NIF_Distribuidor INT NOT NULL,
+  Matricula_Carrinha varchar(64) NOT NULL,
+
+  PRIMARY KEY (NIF_Distribuidor),
+  FOREIGN KEY (NIF_Distribuidor) REFERENCES Higiliquidos.Funcionario(NIF_Funcionario),
+  FOREIGN KEY (Matricula_Carrinha) REFERENCES Higiliquidos.Carrinha(matricula)
+);
+
+
+CREATE TABLE Higiliquidos.Encomenda_Distribuidor (
+  NIF_Distribuidor INT NOT NULL,
+  ID_Encomenda INT NOT NULL,
+  Data_Enc DATE NOT NULL,
+
+  PRIMARY KEY (Data_Enc,  ID_Encomenda),
+  FOREIGN KEY (NIF_Distribuidor) REFERENCES Higiliquidos.Distribuidor(NIF_Distribuidor),
+  FOREIGN KEY (ID_Encomenda) REFERENCES Higiliquidos.Encomenda(ID)
+
+);
+
+
+CREATE TABLE Higiliquidos.Carrinha_Distribuidor (
+  Matricula_Carrinha varchar(64) NOT NULL,
+  NIF_Distribuidor INT NOT NULL,
+  data_In DATE NOT NULL,
+  data_Out DATE,
+
+  PRIMARY KEY (data_In, data_Out, Matricula_Carrinha, NIF_Distribuidor),
+  FOREIGN KEY (Matricula_Carrinha) REFERENCES Higiliquidos.Carrinha(matricula),
+  FOREIGN KEY (NIF_Distribuidor) REFERENCES Higiliquidos.Distribuidor(NIF_Distribuidor)
+);
+
+
+CREATE TABLE Higiliquidos.Gerente (
+  Permissao_Venda BOOLEAN NOT NULL,
+  Permissao_Gerenciar BOOLEAN NOT NULL,
+  Permissao_Distribuir BOOLEAN NOT NULL,
+  NIF_Gerente INT NOT NULL,
+  Matricula_Carrinha varchar(64) NOT NULL,
+
+  FOREIGN KEY (NIF_Gerente) REFERENCES Higiliquidos.Funcionario(NIF_Funcionario),
+  PRIMARY KEY (NIF_Gerente)
+);
+
+
+CREATE TABLE Higiliquidos.Encomenda_Produto (
+  ID_Encomenda INT,
+  ID_Produto INT,
+  quantidade INT,
+  FOREIGN KEY (ID_Encomenda) REFERENCES Higiliquidos.Encomenda(ID),
+  FOREIGN KEY (ID_Produto) REFERENCES Higiliquidos.Produto(ID),
+  PRIMARY KEY (quantidade, ID_Encomenda, ID_Produto)
+);
+
+
+
+
+
+
 
 
 
--- Projeto/DML.sql
+++ Projeto/DML.sql
@@ -1,30 +1,49 @@
-INSERT INTO Higiliquidos.Carrinha(matricula,ano,marca,combustivel,peso)
-VALUES
-  ("AA-09-BD",2018,"Renault","Diesel",1160),
-  ("CD-89-AK", 2014, "Mercedes","Gasolina",5000),
-  ("EF-34-RT", 2019, "Fiat","Gasolina",1500),
-  ("GH-56-LM", 2016, "Ford","Diesel",2000),
-  ("IJ-78-NP", 2017, "Toyota","Gasolina",2500);
-  ("KL-90-QR", 2020, "Dacia","Gasolina",3000),
-  ("OP-45-UV", 2018, "Peugeot","Gasolina",1800),
-  ("QR-67-WX", 2016, "Citroen","Diesel",1900);
+INSERT INTO Higiliquidos.Pessoa(NIF, nome, Data_de_Nascimento, email, morada, ContactoTelefonico) VALUES
+(294455765, "João Antonio", "05/10/1983", 'joaoant@gmail.com', "Rua Vou AliJaVenho", "931764532"),
+(287649264, "Maria Silva", "12/03/1990", 'maria.silva@gmail.com', "Avenida Principal", "987654321"),
+(987654321, "Pedro Santos", "07/07/1985", 'pedro.santos@gmail.com', "Rua das Flores", "777888999"),
+(295748365, "Ana Sousa", "22/09/1995", 'ana.sousa@gmail.com', "Travessa das Oliveiras", "111222333"),
+(111111111, "Carlos Costa", "19/05/1978", 'carlos.costa@gmail.com', "Rua dos Amigos", "555444333"),
+(222222222, "Sofia Rodrigues", "14/08/1992", 'sofia.rodrigues@gmail.com', "Avenida Central", "666777888"),
+(999999999, "Rita Pereira", "28/11/1989", 'rita.pereira@gmail.com', "Praça da Liberdade", "222333444"),
+(888888888, "Luis Mendes", "02/02/1987", 'luis.mendes@gmail.com', "Rua Nova", "777111333"),
+(777777777, "Andre Almeida", "15/06/1998", 'andre.almeida@gmail.com', "Travessa dos Cravos", "999444555"),
+(666666666, "Marta Gomes", "10/09/1993", 'marta.gomes@gmail.com', "Avenida da Paz", "123456789"),
+(444444444, "Ricardo Ferreira", "25/12/1980", 'ricardo.ferreira@gmail.com', "Rua dos Sonhos", "444555666"),
+(333333333, "Inês Pereira", "18/07/1996", 'ines.pereira@gmail.com', "Praça dos Leões", "789123456"),
+(222222333, "Paulo Marques", "30/03/1984", 'paulo.marques@gmail.com', "Rua das Gaivotas", "987654321"),
 
 
-INSERT INTO Higiliquidos.Empresa(nome,email,endereco,ContactoTelefonico)
-VALUES
-  ("Higilíquidos","higiliquidos@sapo.pt","Rua Barroco da Cela 8, Arrifana",271 963 349);
 
-INSERT INTO Higiliquidos.Armazem(id,tamanho,endereco)
-VALUES
-  (1,2100,"Rua 31 Janeiro, Arrifana"),
-  (2,1500,"Rua 31 Janeiro, Arrifana"),
-  (3,300,"Rua 31 Janeiro, Arrifana"),
-  (4,150,"Rua Barroco da Cela 8, Arrifana"),
-  (5,400,"Rua Barroco da Cela 8, Arrifana");
 
-INSERT INTO Higiliquidos.Empresa(id,nome,email,morada,ContactoTelefonico,NIF,Data_de_Nascimentores)
-VALUES
-  (0,"João Antonio","toninhodasbabes@gmail.com","Rua Vou AliJaVenho",936963455,294455765,"05/10/1983"),
-  (1,"Miguel Antonio","miguela@gmail.com","Rua Vou AliJaVenho",936963455,294455765,"05/10/1983");
+
+
+
+
+
+INSERT INTO Higiliquidos.Carrinha(matricula,ano,marca,combustivel,peso) VALUES
+("AA-09-BD",2018,"Renault","Diesel",1160),
+("CD-89-AK", 2014, "Mercedes","Gasolina",5000),
+("EF-34-RT", 2019, "Fiat","Gasolina",1500),
+("GH-56-LM", 2016, "Ford","Diesel",2000),
+("IJ-78-NP", 2017, "Toyota","Gasolina",2500);
+("KL-90-QR", 2020, "Dacia","Gasolina",3000),
+("OP-45-UV", 2018, "Peugeot","Gasolina",1800),
+("QR-67-WX", 2016, "Citroen","Diesel",1900);
+
+
+INSERT INTO Higiliquidos.Empresa(nome,email,endereco,ContactoTelefonico) VALUES
+("Higilíquidos","higiliquidos@sapo.pt","Rua Barroco da Cela 8, Arrifana",271 963 349);
+
+INSERT INTO Higiliquidos.Armazem(ID,tamanho,endereco) VALUES
+(1,2100,"Rua 31 Janeiro, Arrifana"),
+(2,1500,"Rua 31 Janeiro, Arrifana"),
+(3,300,"Rua 31 Janeiro, Arrifana"),
+(4,150,"Rua Barroco da Cela 8, Arrifana"),
+(5,400,"Rua Barroco da Cela 8, Arrifana");
+
+INSERT INTO Higiliquidos.Empresa(ID,nome,email,morada,ContactoTelefonico,NIF,Data_de_Nascimentores) VALUES
+(0,"João Antonio","toninhodasbabes@gmail.com","Rua Vou AliJaVenho",936963455,294455765,"05/10/1983"),
+(1,"Miguel Antonio","miguela@gmail.com","Rua Vou AliJaVenho",936963455,294455765,"05/10/1983");
 
 
